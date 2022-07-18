22941a32ea3ea5a593cc98d831c032b479395606
dohyun <pandha12@naver.com>
2022-07-18 17:24:06 +0900

    a : 1번 릴레이 (사용안함) b : 2번 릴레이 (솔레노이드 #1) c : 3번 릴레이 (솔레노이드 #2) d : 4번 릴레이 (펌프)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import serial
 import time
+import tkinter as tk
 
 py_serial = serial.Serial(port="/dev/cu.usbserial-1140",baudrate = 9600)
+
 
 while True :
 
@@ -14,6 +16,31 @@ while True :
 
         response = py_serial.readline()
         print(response[:len(response) - 1].decode())
+
+main_disp = tk.Tk()
+main_disp.geometry("1000x800")
+main_disp.resizable(True,True)
+
+valve_1_label = tk.Label(main_disp,text="솔레노이드 밸브 #1 :")
+valve_1_label.grid(row=0, column=0)
+valve_1_status = tk.Label(main_disp,textvariable = response)
+valve_1_status.grid(row=0,column=1)
+
+valve_1_label = tk.Label(main_disp,text="솔레노이드 밸브 #2 :")
+valve_1_label.grid(row=1, column=0)
+valve_1_status = tk.Label(main_disp,text="정상")
+valve_1_status.grid(row=1,column=1)
+
+valve_1_label = tk.Label(main_disp,text="펌프 작동 상태 :")
+valve_1_label.grid(row=2, column=0)
+valve_1_status = tk.Label(main_disp,text="정상")
+valve_1_status.grid(row=2,column=1)
+
+main_disp.mainloop()
+
+
+
+
 
 
 
