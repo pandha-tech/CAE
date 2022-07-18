ea3f9d2d213cf77966638aad51f3e003aa017ce0
dohyun <pandha12@inha.edu>
2022-07-18 23:53:08 +0900

    20220718 23:33

--- main.py
+++ main.py
@@ -24,22 +24,54 @@ main_disp.geometry("1000x800")
 main_disp.resizable(True,True)
 
 operation_status_label = tk.Label(main_disp,text="Operation Status :")
-operation_status_label.grid(row=0, column=0)
+operation_status_label.grid(row=0, column=0, columnspan=3)
 
-valve_1_label = tk.Label(main_disp,text="솔레노이드 밸브 #1 :")
-valve_1_label.grid(row=1, column=0)
-valve_1_status = tk.Label(main_disp,text = "정상")
-valve_1_status.grid(row=1,column=1)
+flow_rate_status_label = tk.Label(main_disp, text="Flow rate : ")
+flow_rate_status_label.grid(row=0, column=4, columnspan=3)
+flow_rate = tk.Label(main_disp, text="25.3 L/M", bg="white", fg="blue")
+flow_rate.grid(row=0, column=8)
 
-valve_2_label = tk.Label(main_disp,text="솔레노이드 밸브 #2 :")
-valve_2_label.grid(row=2, column=0)
-valve_2_status = tk.Label(main_disp,text="정상")
-valve_2_status.grid(row=2,column=1)
+pressure_status_label = tk.Label(main_disp, text="Pressure : ")
+pressure_status_label.grid(row=0, column=9)
 
-pump_label = tk.Label(main_disp,text="펌프 작동 상태 :")
-pump_label.grid(row=3, column=0)
-pump_status = tk.Label(main_disp,text="정상")
-pump_status.grid(row=3,column=1)
+temp_status_label = tk.Label(main_disp, text="Temperature : ")
+temp_status_label.grid(row=0, column=10)
+
+interval_label = tk.Label(main_disp,text="Interval #1 Status : ")
+interval_label.grid(row=1, column=0)
+interval_status = tk.Label(main_disp,text="NORMAL", bg="white", fg="blue")
+interval_status.grid(row=1, column=1)
+
+
+valve_1_label = tk.Label(main_disp,text="Valve #1 Status :")
+valve_1_label.grid(row=2, column=0)
+valve_1_status = tk.Label(main_disp,text="NORMAL", bg="white", fg="blue")
+valve_1_status.grid(row=2,column=1)
+
+valve_2_label = tk.Label(main_disp,text="Valve #2 Status :")
+valve_2_label.grid(row=3, column=0)
+valve_2_status = tk.Label(main_disp,text="NORMAL", bg="white", fg="blue")
+valve_2_status.grid(row=3,column=1)
+
+pump_label = tk.Label(main_disp,text="Pump Status :")
+pump_label.grid(row=4, column=0)
+pump_status = tk.Label(main_disp,text="NORMAL", bg="white", fg="blue")
+pump_status.grid(row=4,column=1)
+
+interval_open = tk.Button(main_disp, text="INTERVAL #1 OPEN")
+interval_open.grid(row=5, column=0)
+interval_close = tk.Button(main_disp, text="INTERVAL #1 CLOSE")
+interval_close.grid(row=5, column=1)
+
+valve_1_on_button = tk.Button(main_disp,text="Valve #1 ON")
+valve_1_on_button.grid(row=6, column=0)
+valve_1_off_button = tk.Button(main_disp,text="Valve #1 OFF")
+valve_1_off_button.grid(row=7, column=0)
+
+valve_2_on_button = tk.Button(main_disp,text="Valve #2 ON")
+valve_2_on_button.grid(row=6, column=1)
+valve_2_off_button = tk.Button(main_disp,text="Valve #2 OFF")
+valve_2_off_button.grid(row=7, column=1)
 
 main_disp.mainloop()
 
